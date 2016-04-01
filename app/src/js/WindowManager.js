2e9ac1fb7f569ecc65d8726db0acf8eada95d492
José A. Múrcia Andrés <joamuran@gmail.com>
2016-04-01 14:17:20 +0200

    wip

--- app/src/js/WindowManager.js
+++ app/src/js/WindowManager.js
@@ -1,46 +1,30 @@
 gui=require('nw.gui');
+var exec = require('child_process').exec;
+var dbus = require('dbus-native');
+var bus = dbus.systemBus();
 
-var params = {
+/*var params = {
   toolbar: false,
   frame: false,
   transparent: false,
   resizable: false,
   show_in_taskbar: false,
   show: true};
-var window = gui.Window.open('main2.html', params);
+var window = gui.Window.open('main2.html', params);*/
+
 
 //window.show();
 //window.focus();
 
 // show panel when click in tray
 
-/*console.log("0");
 
 function WindowManager (){
-  console.log("1");
   this.window=gui.Window.get();
-  console.log("2");
-  console.log("3");
-  this.window.setShowInTaskbar(false);
-  this.window.show(true);
-  console.log("4");
-  
-  console.log("5");
-  
-  this.shortcut = new gui.Shortcut(option);
-  var self=this;
-
-  this.window.on("blur", function(){
-      //self.window.hide();
-      //self.window.show=true;
-      alert("lose focus");
-
-  })
-  // Start hidden
-  //this.window.hide();    
+  this.window.show();
+  // Hide from taskbar
+  child = exec('wmctrl -r "LliureX Launchpad" -b toggle,skip_taskbar', function (error, stdout, stderr) {});
 }
 
-var wm= new WindowManager ();
+var wm=new WindowManager ();
 
-
-*/
